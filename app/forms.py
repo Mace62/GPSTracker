2ebf4af8e378b6401fd63734d53d43e629d3c662
kakaorigby <sc22h2s@leeds.ac.uk>
2024-02-16 21:21:02 +0000

    Minor changes to forms + modularised login/reg page for later design changes

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, StringField, PasswordField, FloatField, IntegerField, EmailField
+from wtforms import TextAreaField, StringField, PasswordField, FloatField, IntegerField, EmailField, SubmitField
 from wtforms.validators import DataRequired, ValidationError, Email
 
 class LoginForm(FlaskForm):
-    username = TextAreaField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
 
 
 class RegisterForm(FlaskForm):
-    username = TextAreaField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm = PasswordField('Confirm Password', validators=[DataRequired()])
-    first_name = TextAreaField('First Name', validators=[DataRequired()])
-    last_name = TextAreaField('Last Name', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
     email = EmailField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Register')
 
     def validate_password(form, field):
         password = field.data
