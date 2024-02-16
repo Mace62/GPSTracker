4eb7de927b0182a9eb74133a08ff22366e81616e
kakaorigby <sc22h2s@leeds.ac.uk>
2024-02-16 16:46:28 +0000

    added DB model for User, and needed files

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), index = True, unique = True, nullable = False)
+    firstname = db.Column(db.String(120), index = True, nullable = False)
+    lastname = db.Column(db.String(120), index = True, nullable = False)
+    email = db.Column(db.String(120), index = True, unique = True, nullable = False)
+    password = db.Column(db.String(120), index = True, nullable = False)
+
+
