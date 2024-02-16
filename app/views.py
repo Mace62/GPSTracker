c69e19bc2b4e34f03915bdd2fb7e6e7c86fcc2bc
kakaorigby <sc22h2s@leeds.ac.uk>
2024-02-16 16:27:59 +0000

    additional environment setup

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+from flask import *
 from app import app
+from flask import render_template, flash, request
 
 @app.route('/')
 def index():
-    return "Hello World!!!"
+    return render_template('index.html')
