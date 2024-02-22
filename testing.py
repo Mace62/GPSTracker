190e574072c353232273eb181b64f3b1c7384bd8
kakaorigby <sc22h2s@leeds.ac.uk>
2024-02-22 20:43:06 +0000

    added register unit test

--- testing.py
+++ testing.py
@@ -30,6 +30,7 @@ class CustomTestResult(unittest.TextTestResult):
         super().addError(test, err)
         print(f"  -> Error: {err[1]}")
 
+
 class TestRegistration(TestCase):
 
     def create_app(self):
@@ -50,137 +51,22 @@ class TestRegistration(TestCase):
         """Test user registration."""
         response = self.client.post('/register', data=dict(
             username='testuser',
-            password='testpassword',
-            confirm_password='testpassword'
+            password='Testpassword!',
+            confirm='Testpassword!',
+            email='example@example.com',
+            first_name='Test',
+            last_name='User'
         ), follow_redirects=True)
 
         self.assertEqual(response.status_code, 200)
         self.assertIn(
-            b'Your account has been created! You can now log in.', response.data)
+            b'Registered and logged in successfully.', response.data)
 
         # Check if the user is now in the database
         user = User.query.filter_by(username='testuser').first()
         self.assertIsNotNone(user)
         self.assertEqual(user.username, 'testuser')
 
-
-class TestLogin(TestCase):
-
-    def create_app(self):
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-        return app
-
-    def setUp(self):
-        db.create_all()
-        self.client = app.test_client()
-
-        # Create a test user for login
-        hashed_password = bcrypt.generate_password_hash("testpassword")
-        test_user = User(username='testuser', password=hashed_password)
-        db.session.add(test_user)
-        db.session.commit()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
-    def test_login(self):
-        """Test user login."""
-        response = self.client.post('/login', data=dict(
-            username='testuser',
-            password='testpassword'
-        ), follow_redirects=True)
-
-        self.assertEqual(response.status_code, 200)
-
-        decoded_response = response.data.decode('utf-8')
-        self.assertIn('Hello there', decoded_response)
-
-
-class TestIncorrectLogin(TestCase):
-
-    def create_app(self):
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-        return app
-
-    def setUp(self):
-        db.create_all()
-        self.client = app.test_client()
-
-        # Create a test user for login
-        hashed_password = bcrypt.generate_password_hash("testpassword")
-        test_user = User(username='testuser', password=hashed_password)
-        db.session.add(test_user)
-        db.session.commit()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
-    def test_login(self):
-        """Test incorrect user login."""
-        response = self.client.post('/login', data=dict(
-            username='testuser',
-            password='testpassword1'
-        ), follow_redirects=True)
-
-        self.assertEqual(response.status_code, 200)
-
-        decoded_response = response.data.decode('utf-8')
-        self.assertIn(
-            'Incorrect username or password. Please try again.', decoded_response)
-
-
-class TestLogout(TestCase):
-
-    def create_app(self):
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-        return app
-
-    def setUp(self):
-        db.create_all()
-        self.client = app.test_client()
-
-        # Create a test user
-        hashed_password = bcrypt.generate_password_hash("testpassword")
-        test_user = User(username='testuser', password=hashed_password)
-        db.session.add(test_user)
-        db.session.commit()
-
-        # Log in the test user
-        response = self.client.post('/login', data=dict(
-            username='testuser',
-            password='testpassword'
-        ), follow_redirects=True)
-
-        self.assertEqual(response.status_code, 200)
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
-    def test_logout(self):
-        """Test user logout."""
-        # Log out the user
-        response = self.client.get('/logout', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
-
-        # Check if the user is no longer authenticated
-        with self.client as c:
-            response = c.get('/profile', follow_redirects=True)
-            self.assertEqual(response.status_code, 200)
-            self.assertIn(b'Login Page', response.data)
-
-
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestRegistration)
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestLogin))
-    suite.addTests(unittest.TestLoader(
-    ).loadTestsFromTestCase(TestIncorrectLogin))
     unittest.TextTestRunner(resultclass=CustomTestResult).run(suite)
