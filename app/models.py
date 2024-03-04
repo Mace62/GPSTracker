fc1df9b6d79a55b46b8515730e95ba70f13bda0e
sr-62 <sc222sr@leeds.ac.uk>
2024-03-04 17:24:41 +0000

    Re-organized code

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 from flask_login import UserMixin
 from app import db
 
+import gpxpy
+import gpxpy.gpx
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True, nullable = False)
@@ -16,10 +19,25 @@ class Admin(db.Model):
 ## Classes for GPX parsing ##
 
 class GPXFile:
-    def __init__(self, name):
+    def __init__(self, name, filepath):
         self.name = name
+        self.filepath = filepath
         self.tracks = []
         self.waypoints = []
+
+        ## Code for parsing in from file
+        with open(self.filepath, 'r') as gpx_file:
+            data = gpxpy.parse(gpx_file)
+
+        for waypoint in data.waypoints:
+            self.waypoints.append(GPXPoint(waypoint.name, waypoint.latitude, waypoint.longitude, waypoint.elevation, waypoint.time))
+
+        for route in data.routes:
+            route_name = route.name if route.name else "Unnamed Route"
+            track = GPXTrack(route_name)
+            for point in route.points:
+                track.points.append(GPXPoint(point.name, point.latitude, point.longitude, point.elevation, point.time))
+            self.tracks.append(track)
 
     def display_info(self):
         print(f"GPX File: {self.name}")
@@ -49,4 +67,4 @@ class GPXPoint:
         self.time = time
 
     def display_info(self):
-        print(f"    Point: {self.name}, Location: ({self.latitude}, {self.longitude}), Elevation: {self.elevation}, Time: {self.time}, Description: {self.description}")
+        print(f"    Point: {self.name}, Location: ({self.latitude}, {self.longitude}), Elevation: {self.elevation}, Time: {self.time}")
